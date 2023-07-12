da2789594d7c18aef6964cf546d894f372b2f4dd
komarov <arty-komarov@ya.ru>
2023-07-12 18:27:10 +0300

    Add entrypoint

--- src/game21.cpp
+++ src/game21.cpp
@@ -1,7 +1,15 @@
+#include "CardDeck.hpp"
 #include <iostream>
+#include <array>
+
+using namespace game21;
 
 int main(int argc, char const *argv[])
 {
-    std::cout << "Game21\n";
+    for (auto &&card : CardDeck::deck)
+    {
+        std::cout << static_cast<std::string>(card) << "\n";
+    }
+    
     return 0;
 }
