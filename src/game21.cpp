1e70a12979d2193bda022f687a8151dfe29dac59
Komarov Artem <arty-komarov@yandex.ru>
2023-07-13 00:47:52 +0300

    Entry point card sum check

--- src/game21.cpp
+++ src/game21.cpp
@@ -1,15 +1,19 @@
 #include "CardDeck.hpp"
-#include <iostream>
 #include <array>
+#include <iostream>
 
 using namespace game21;
 
 int main(int argc, char const *argv[])
 {
+    int sum = 0;
     for (auto &&card : CardDeck::deck)
     {
+        sum += card;
         std::cout << static_cast<std::string>(card) << "\n";
     }
-    
+
+    std::cout << "Card sum:" << sum << std::endl;
+
     return 0;
 }
