7bb62221eec8bec5dbd584c93ac0927c8d8b9f3b
Komarov Artem <arty-komarov@yandex.ru>
2023-07-15 15:00:50 +0300

    AskPlayerBid realize

--- src/Game21/States/AskPlayerBid.cpp
+++ src/Game21/States/AskPlayerBid.cpp
@@ -1,11 +1,37 @@
-#include "Game.hpp"
 #include "States/AskPlayerBid.hpp"
+#include "Game.hpp"
+#include <iomanip>
+#include <sstream>
 
 namespace game21
 {
-    Scene AskPlayerBid::execute(Game __attribute_maybe_unused__ *context)
+    Scene AskPlayerBid::execute(Game __attribute_maybe_unused__ *game)
     {
-        std::cout<<"AskPlayerBid\n";
+        game->waitFor(1500);
+        game->clearScreen();
+
+        std::ostringstream msg;
+        msg << "Укажите вашу ставку (не более "
+            << std::fixed << std::setprecision(2)
+            << game->mPlrBalance
+            << " Ctrl+D выход):\n";
+
+        while (true)
+        {
+            game->getInput(
+                game->mPlrBid, msg.str().c_str(),
+                "Не корректный ввод, попробуйте еще раз (Ctrl+D выход):\n");
+            if (game->mPlrBid <= game->mPlrBalance)
+            {
+                break;
+            }
+            game->mOuterr << "Ставка: " << game->mPlrBid
+                          << " не может превышать баланс: "
+                          << game->mPlrBalance << "\n";
+        }
+
+        game->mPlrBalance -= game->mPlrBid;
+
         return Scene::DillerGetsCards;
     }
 
