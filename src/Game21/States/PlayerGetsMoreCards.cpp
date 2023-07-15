91d648dfe6bca10486bf81502df46e959a214353
Komarov Artem <arty-komarov@yandex.ru>
2023-07-15 17:57:23 +0300

    PlayerGetsMoreCards completed

--- src/Game21/States/PlayerGetsMoreCards.cpp
+++ src/Game21/States/PlayerGetsMoreCards.cpp
@@ -1,12 +1,37 @@
 #include "States/PlayerGetsMoreCards.hpp"
+#include "CardDeck.hpp"
 #include "Game.hpp"
 
 namespace game21
 {
-    Scene PlayerGetsMoreCards::execute(Game __attribute_maybe_unused__ *context)
+    Scene PlayerGetsMoreCards::execute(Game __attribute_maybe_unused__ *game)
     {
-        std::cout << "PlayerGetsMoreCards\n";
-        return Scene::DillerGetsMoreCards;
+        if (game->mPlrHand.getValue() > 21)
+        {
+            return Scene::PlayerOverhead;
+        }
+
+        game->waitFor(1500);
+        game->clearScreen();
+        game->showInfo();
+
+        std::string yORn;
+
+        game->getInput(
+            yORn,
+            "Взять еще карту - \"д\", отказаться любая кл. (Ctrl+D выход)?\n",
+            "Не корректный ввод, попробуйте еще раз (Ctrl+D выход):\n");
+
+        if (yORn != "y" && yORn != "д")
+        {
+            return Scene::DillerGetsMoreCards;
+        }
+
+        auto &card = CardDeck::getCard();
+        game->mPlrHand.addCard(&card);
+        game->mOutput << "Игрок получает карту: " << card.toString() << "\n";
+
+        return Scene::PlayerGetsMoreCards;
     }
 
 } // namespace game21
