5cfd3c47ffcdd2d01ce6f5e86b63d15832a434c3
Komarov Artem <arty-komarov@yandex.ru>
2023-07-15 17:56:57 +0300

    DillerGetsCards complete

--- src/Game21/States/DillerGetsCards.cpp
+++ src/Game21/States/DillerGetsCards.cpp
@@ -1,5 +1,6 @@
-#include "Game.hpp"
 #include "States/DillerGetsCards.hpp"
+#include "CardDeck.hpp"
+#include "Game.hpp"
 
 namespace game21
 {
@@ -9,10 +10,17 @@ namespace game21
         game->clearScreen();
         game->showInfo();
 
-        
+        auto &card1 = CardDeck::getCard();
+        auto &card2 = CardDeck::getCard();
 
-        std::cout << "DillerGetsCards\n";
-        return Scene::Finish; // Scene::PlayerGetsCards;
+        game->mOutput << "Диллер получает карты: "
+                      << card1.toString() << " "
+                      << card2.toString() << "\n";
+
+        game->mDlrHand.addCard(&card1);
+        game->mDlrHand.addCard(&card2);
+
+        return Scene::PlayerGetsCards;
     }
 
 } // namespace game21
